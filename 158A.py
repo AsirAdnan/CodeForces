33816b6bc0d44ebddd6a357587e5b4fa3d3115d5
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-08-19 20:44:49 +0600

    Update 158A.py

--- 158A.py
+++ 158A.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 x,y=[int(i) for i in input().split()]
 l=[int(j) for j in input().split()]
 x=l[y-1]
@@ -12,10 +6,3 @@ for k in l:
     if k>=x and k>0:
         count+=1
 print(count)
-
-
-# In[ ]:
-
-
-
-
