ca268a83e88c556d8bd7955afc153d9b3ee6fa75
Asir Adnan <106885381+AsirAdnan@users.noreply.github.com>
2022-09-16 22:34:34 +0600

    Create 1339A.cpp

---
+++ 1339A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t,a;
+    cin>>t;
+    while(t--){
+        cin>>a;
+        cout<<a<<"\n";
+    }
+}
